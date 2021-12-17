b779e569362e6c322b16e47be9e3654959c036c5
Luke Kreft <luke@mirkre.com>
2021-12-17 17:32:20 -0500

    stage 8

--- parasite.sh
+++ parasite.sh
@@ -3,25 +3,23 @@
 
 
 
-generate_response() {
-    echo "HTTP/1.1"
+echo "HTTP/1.1"
 
-    case "$uri" in
-        (/)
-            echo "Content-Type: text/html"
-            echo 
-            echo "<html>"
-            echo "<head>"
-            echo "<title>Parasite</title>"
-            echo "</head>"
-            echo "<body>"
-            echo "<div style='max-width: 800px; margin: auto;'>"
-            echo "<h1>Hello</h1>"
-            echo "<hr/>"
-            echo "<p>This is the home page</p>"
-            echo "</div>"
-            echo "</body>"
-            echo "</html>"
-        ;;
-    esac
-}
+case "$uri" in
+    (/)
+        echo "Content-Type: text/html"
+        echo 
+        echo "<html>"
+        echo "<head>"
+        echo "<title>Parasite</title>"
+        echo "</head>"
+        echo "<body>"
+        echo "<div style='max-width: 800px; margin: auto;'>"
+        echo "<h1>Hello</h1>"
+        echo "<hr/>"
+        echo "<p>This is the home page</p>"
+        echo "</div>"
+        echo "</body>"
+        echo "</html>"
+    ;;
+esac
